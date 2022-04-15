16e16575e3c0c48202c657a0bd522d2f0d5d5aa9
Anthony Norderhaug <anorderhaug0480@sdsu.edu>
2022-04-15 10:10:01 -0700

    reworking controller

--- 2D Game/Assets/Characters/Player/player2DController.cs
+++ 2D Game/Assets/Characters/Player/player2DController.cs
@@ -5,6 +5,7 @@ public class player2DController : MonoBehaviour
 
     public float speed = 2;
     public float jumpPower = 5;
+    public float movement;
 
     private Rigidbody2D _rigidbody;
     private Animator animator;
@@ -18,15 +19,46 @@ public class player2DController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        var movement = Input.GetAxis("Horizontal");
-        animator.SetFloat("speed", Mathf.Abs(movement));
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
+        if (!inAir() || !isCrouched()) {
+            movement = Input.GetAxis("Horizontal");
 
-        if (!Mathf.Approximately(0, movement))
-            transform.rotation = movement > 0 ? Quaternion.identity : Quaternion.Euler(0, 180, 0);
-
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
-            _rigidbody.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+            if (Input.GetButtonDown("Jump"))
+                _rigidbody.AddForce(new Vector2(0, jumpPower));
         
+        } else {
+            movement = 0;
+        }
+
+        animator.SetFloat("speed", Mathf.Abs(movement));
+        
+    }
+
+    bool inAir() {
+        if (Mathf.Abs(_rigidbody.velocity.y) < 0.001f) {
+            animator.SetBool("inAir", false);
+            return false;
+        } else {
+            animator.SetBool("inAir", true);
+            return true;
+        }
+    }
+
+    bool isCrouched() {
+        if (Input.GetButtonDown("Crouch")) {
+            animator.SetBool("crouched", true);
+            return true;
+        } else {
+            animator.SetBool("crouched", false);
+            return false;
+        }
+    }
+
+    void FixedUpdate () 
+    {
+        // change position
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
+        // change direction
+        if (!Mathf.Approximately(0, movement)) 
+            transform.rotation = movement > 0 ? Quaternion.identity : Quaternion.Euler(0, 180, 0);
     }
 }

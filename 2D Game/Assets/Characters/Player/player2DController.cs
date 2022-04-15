8d05a76e26eb2f37ab68fa997fc0977bf93650b2
Anthony Norderhaug <anorderhaug0480@sdsu.edu>
2022-04-15 14:01:03 -0700

    reworked controller + more ani

--- 2D Game/Assets/Characters/Player/player2DController.cs
+++ 2D Game/Assets/Characters/Player/player2DController.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 public class player2DController : MonoBehaviour
 {
 
-    public float speed = 2;
-    public float jumpPower = 5;
-    public float movement;
+    public float speed = 1f;
+    public int jumpPower = 10;
+    public Collider2D standing;
+    public Collider2D crouch;
 
+
+    private bool crouchFlag;
+    private float movement;
+    private Vector3 m_Velocity = Vector3.zero;
     private Rigidbody2D _rigidbody;
     private Animator animator;
 
@@ -14,51 +19,55 @@ public class player2DController : MonoBehaviour
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        crouchFlag = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (!inAir() || !isCrouched()) {
-            movement = Input.GetAxis("Horizontal");
-
-            if (Input.GetButtonDown("Jump"))
-                _rigidbody.AddForce(new Vector2(0, jumpPower));
-        
+        isCrouched();
+        if (crouchFlag) {
+            movement = Input.GetAxisRaw("Horizontal") * (speed/3);
         } else {
-            movement = 0;
+            movement = Input.GetAxisRaw("Horizontal") * speed;
         }
+        movement *= Time.fixedDeltaTime;
+
+        standing.enabled = !animator.GetBool("crouched");
+        crouch.enabled = !(standing.enabled);
+
 
         animator.SetFloat("speed", Mathf.Abs(movement));
+        animator.SetFloat("y", _rigidbody.velocity.y);
+
+
+        if (Mathf.Abs(_rigidbody.velocity.y) < 0.0001f)  {
+            if (Input.GetButtonDown("Jump"))
+                _rigidbody.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+        
+        } 
         
     }
 
-    bool inAir() {
-        if (Mathf.Abs(_rigidbody.velocity.y) < 0.001f) {
-            animator.SetBool("inAir", false);
-            return false;
-        } else {
-            animator.SetBool("inAir", true);
-            return true;
-        }
-    }
-
-    bool isCrouched() {
+    void isCrouched() {
         if (Input.GetButtonDown("Crouch")) {
             animator.SetBool("crouched", true);
-            return true;
-        } else {
+            crouchFlag = true;
+
+        } else if (Input.GetButtonUp("Crouch")) {
             animator.SetBool("crouched", false);
-            return false;
+            crouchFlag = false;
         }
     }
 
     void FixedUpdate () 
     {
-        // change position
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
-        // change direction
+
+        Vector3 targetVelocity = new Vector2(movement, _rigidbody.velocity.y);
+        _rigidbody.velocity = Vector3.SmoothDamp(_rigidbody.velocity, targetVelocity, ref m_Velocity, .1f);
+        
         if (!Mathf.Approximately(0, movement)) 
             transform.rotation = movement > 0 ? Quaternion.identity : Quaternion.Euler(0, 180, 0);
     }
+
 }
